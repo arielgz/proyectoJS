ba01ce903d176b1e01b255131bfeae59e6d34d05
Arielgz <arselocura@gamil.com>
2022-08-22 22:45:15 -0300

    integrando fetch

---
+++ javaScript/remeras.js
@@ -0,0 +1,46 @@
+const remerasContainer = document.querySelector("#containerCard-remeras");
+
+
+const respuestaApi = () => {
+    fetch("../indumentaria.json")
+    .then(respuesta =>respuesta.json())
+    .then(data => {
+     mostrarAHtml(data.productos);
+
+    })
+    
+
+
+}
+respuestaApi()
+
+const mostrarAHtml = (array) => {
+    array.forEach(elemento => {
+        remerasContainer.innerHTML +=`
+        <div class="content-card">
+                <div class="img-card">
+                    <img src=${elemento.img}>
+                </div>
+                <div class="texto-card">
+                    <h3>${elemento.nombre}</h3>
+                    <span>$${elemento.precio.toFixed(3)}</span>
+                </div>
+                <div class="btn-card"><button  id="btn-agregar">Agregar al carrito</button>
+                </div>
+            </div> `
+        
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
